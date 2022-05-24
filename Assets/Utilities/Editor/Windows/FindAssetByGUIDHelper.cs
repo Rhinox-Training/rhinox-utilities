a292956366d2c1d0ee21c7c911a2e437829a3dc8
Gaetan Deglorie <gaetan@rhinox.training>
2022-05-24 12:08:12 +0200

    Fix for assetbyguidhelper

--- Assets/Utilities/Editor/Windows/FindAssetByGUIDHelper.cs
+++ Assets/Utilities/Editor/Windows/FindAssetByGUIDHelper.cs
@@ -10,15 +10,24 @@ namespace Rhinox.Utilities.Editor
         {
             EditorInputDialog.Create("GUID", "Find an asset from it's GUID.")
                 .TextField("Guid:", out var guid)
-                .OnAccept(() => { FindAssetByGuid(guid); });
+                .OnAccept(() => { FindAssetByGuid(guid); })
+                .Show();
         }
 
         static void FindAssetByGuid(string searchGuid)
         {
             string path = AssetDatabase.GUIDToAssetPath(searchGuid);
-            if (string.IsNullOrEmpty(path)) return;
+            if (string.IsNullOrEmpty(path))
+            {
+                Debug.LogError($"Asset with GUID {searchGuid} not found...");
+                return;
+            }
             var obj = AssetDatabase.LoadAssetAtPath<Object>(path);
-            if (obj == null) return;
+            if (obj == null)
+            {
+                Debug.LogError($"Asset with GUID {searchGuid} was not UnityEngine.Object, exiting...");
+                return;
+            }
 
             Selection.activeObject = obj;
             EditorGUIUtility.PingObject(obj);
