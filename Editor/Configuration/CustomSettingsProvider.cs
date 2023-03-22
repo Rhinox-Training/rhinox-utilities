d169b139714e30be1f4ec11d2ba7bec183a72aed
Gaetan Deglorie <gaetan@rhinox.training>
2023-03-22 15:34:34 +0100

    [MOD] Reworked ProjectSettings to enable readonly use in build

---
+++ Editor/Configuration/CustomSettingsProvider.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Rhinox.Lightspeed.Reflection;
+using Rhinox.Utilities.Attributes;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Rhinox.Utilities.Editor.Configuration
+{
+    internal class CustomSettingsProvider : SettingsProvider
+    {
+        private CustomProjectSettings _settingsInstance;
+        private IProjectSettingsDrawer _drawer;
+
+        public CustomSettingsProvider(CustomProjectSettings instance, SettingsScope scope = SettingsScope.User)
+            : base($"Project/Custom/{instance.Name}", scope)
+        {
+            _settingsInstance = instance;
+        }
+
+        public override void OnActivate(string searchContext, VisualElement rootElement)
+        {
+            _drawer = FindDrawer(_settingsInstance);
+
+            if (_drawer != null)
+            {
+                guiHandler = _drawer.OnCustomGUI;
+                activateHandler = _drawer.OnActivate;
+            }
+
+            base.OnActivate(searchContext, rootElement);
+        }
+
+        public IProjectSettingsDrawer FindDrawer(CustomProjectSettings instance)
+        {
+            var types = AppDomain.CurrentDomain.GetDefinedTypesOfType<IProjectSettingsDrawer>();
+            Type applicableType = typeof(CustomProjectSettings);
+            Type targetDrawerType = null;
+            Type instanceType = instance.GetType();
+            foreach (var type in types)
+            {
+                var attr = type.GetCustomAttribute<ProjectSettingsDrawerAttribute>();
+                if (attr == null || attr.SettingsType == null)
+                    continue;
+
+                if (!instanceType.InheritsFrom(attr.SettingsType))
+                    continue;
+
+                if (attr.SettingsType != applicableType && attr.SettingsType.InheritsFrom(applicableType))
+                {
+                    applicableType = attr.SettingsType;
+                    targetDrawerType = type;
+                }
+            }
+
+            IProjectSettingsDrawer drawerInstance = null;
+            if (targetDrawerType != null)
+                drawerInstance = Activator.CreateInstance(targetDrawerType) as IProjectSettingsDrawer;
+            else
+                drawerInstance = new ProjectSettingsDrawer();
+            
+            drawerInstance.LoadTarget(instance);
+            return drawerInstance;
+        }
+        
+        
+        [SettingsProviderGroup]
+        private static SettingsProvider[] CreateSettingsProvidersForInstances()
+        {
+            var settingsProviders = new List<SettingsProvider>();
+            foreach (var customProjectSettings in ProjectSettingsHelper.EnumerateProjectSettings())
+            {
+                var settingsProvider = CreateProvider(customProjectSettings);
+                if (settingsProvider == null)
+                    continue;
+                settingsProviders.Add(settingsProvider);
+            }
+            
+            return settingsProviders.ToArray();
+        }
+
+        private static SettingsProvider CreateProvider(CustomProjectSettings instance)
+        {
+            var provider = new CustomSettingsProvider(instance, SettingsScope.Project);
+
+            // Automatically extract all keywords from the Styles.
+            provider.keywords = instance.GetKeywords();
+            return provider;
+        }
+    }
+}
---
+++ Editor/Configuration/ProjectSettingsImportDrawer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Rhinox.Utilities.Attributes;
+using Rhinox.Utilities.Editor.Configuration;
+using UnityEditor;
+using UnityEngine;
+
+namespace Rhinox.Utilities.Editor
+{
+    [ProjectSettingsDrawer(typeof(CustomProjectSettings<>))]
+    public class ProjectSettingsImportDrawer : ProjectSettingsDrawer
+    {
+        protected override void OnDrawFooter()
+        {
+            base.OnDrawFooter();
+            bool hasEditorFile = HasOldFile();
+            EditorGUI.BeginDisabledGroup(!hasEditorFile);
+            {
+                if (GUILayout.Button("Import"))
+                    TryImport();
+            }
+            EditorGUI.EndDisabledGroup();
+        }
+
+        private bool HasOldFile()
+        {
+            var assetPath = $"Assets/Editor/{_targetObject.GetType().Name}.asset";
+            var settingsGuid = AssetDatabase.AssetPathToGUID(assetPath);
+            return string.IsNullOrWhiteSpace(settingsGuid) || Guid.Parse(settingsGuid) != Guid.Empty;
+        }
+
+        private void TryImport()
+        {
+            var assetPath = $"Assets/Editor/{_targetObject.GetType().Name}.asset";
+            var settings = AssetDatabase.LoadAssetAtPath<CustomProjectSettings>(assetPath);
+
+            throw new NotImplementedException();
+        }
+    }
+}
