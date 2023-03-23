0c53d36077b6da218e75344b4c7442b911208eb5
Gaetan Deglorie <gaetan@rhinox.training>
2023-03-23 11:34:47 +0100

    [ADD] Missing attribute for testing

--- Assets/Tests/RuntimeProjectSettings.cs
+++ Assets/Tests/RuntimeProjectSettings.cs
@@ -1,7 +1,9 @@
 ﻿using Rhinox.Utilities;
+using Rhinox.Utilities.Attributes;
 
 namespace Tests
 {
+    [RuntimeSupport]
     public class RuntimeProjectSettings : CustomProjectSettings<RuntimeProjectSettings>
     {
         public bool Enabled = false;
