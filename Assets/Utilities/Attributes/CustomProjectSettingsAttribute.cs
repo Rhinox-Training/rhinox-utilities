0b78fe4dd6dac9506c23a97c39ec830368f621c1
Jorian Baeke <jorian@rhinox.training>
2023-03-31 11:39:47 +0200

    [MOD] Added Utilities project setting to manage certain forced inspector changes

---
+++ Assets/Utilities/Attributes/CustomProjectSettingsAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Rhinox.Utilities.Attributes
+{
+    public class CustomProjectSettingsAttribute : Attribute
+    {
+        public bool RuntimeSupported { get; set; }
+        public string CustomCollection { get; set; }
+    }
+}
