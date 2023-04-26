e69b2d12a9ca58bc8851dd71fff1b73a279c1a75
Glenn Quintyn <glenn.quintyn@gmail.com>
2023-04-26 16:46:09 +0200

    Comitting current progress

---
+++ Editor/Helpers/ICustomPreProcessBuilder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor.Build.Reporting;
+using UnityEditor.Compilation;
+
+namespace Assets.Utilities.Editor.Helpers
+{
+    internal interface ICustomPreProcessBuilder
+    {
+        void OnPreprocessBuild(BuildReport report);
+
+        void CompilationPipelineOnCompilationStarted(object compilationContext);
+
+        void CompilationPipelineOnAssemblyCompilationFinished(string path, CompilerMessage[] messages);
+
+        void CompilationPipelineOnCompilationFinished(object compilationContext);
+    }
+}
