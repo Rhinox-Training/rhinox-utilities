0387b6adc60c63e737d48e52b36b174777199272
Glenn Quintyn <glenn.quintyn@gmail.com>
2023-04-26 16:46:09 +0200

    Comitting current progress

---
+++ Assets/Tests/TestConfig.cs
@@ -0,0 +1,23 @@
+﻿using Rhinox.Utilities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestConfig : LoadableConfigFile<TestConfig, ConfigFileIniLoader>
+{
+    [ConfigSection("core")]
+    [ConfigCommandArg(nameof(floaty))]
+    public float floaty;
+    [ConfigSection("core")]
+    [ConfigCommandArg(nameof(wholeNumb))]
+    public int wholeNumb;
+    [ConfigSection("core")]
+    [ConfigCommandArg(nameof(boolean))]
+    public bool boolean;
+    [ConfigSection("core")]
+    [ConfigCommandArg(nameof(myName))]
+    public string myName;
+
+    public override string RelativeFilePath => "config.ini";
+}
