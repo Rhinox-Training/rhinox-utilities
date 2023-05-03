cdaebdedaabec728f37959b61b4cd3f16ea0ecf2
Glenn Quintyn <glenn.quintyn@gmail.com>
2023-05-03 10:02:57 +0200

    trail

--- Editor/Helpers/ICustomPreProcessBuilder.cs
+++ Editor/Helpers/ICustomPreProcessBuilder.cs
@@ -7,10 +7,38 @@ namespace Assets.Utilities.Editor.Helpers
     {
         void OnPreprocessBuild(BuildReport report);
 
-        void CompilationPipelineOnCompilationStarted(object compilationContext);
+        //void CompilationPipelineOnCompilationStarted(object compilationContext);
 
-        void CompilationPipelineOnAssemblyCompilationFinished(string path, CompilerMessage[] messages);
+        //void CompilationPipelineOnAssemblyCompilationFinished(string path, CompilerMessage[] messages);
 
-        void CompilationPipelineOnCompilationFinished(object compilationContext);
+        //void CompilationPipelineOnCompilationFinished(object compilationContext);
+    }
+
+    public abstract class CustomPreProcessBuilderWithErrorReport : ICustomPreProcessBuilder
+    {
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            PreprocessBuild(report);
+        }
+
+        public abstract void PreprocessBuild(BuildReport report);
+
+
+        //public void CompilationPipelineOnAssemblyCompilationFinished(string path, CompilerMessage[] messages)
+        //{
+        //    // throw new System.NotImplementedException();
+        //}
+
+        //public void CompilationPipelineOnCompilationFinished(object compilationContext)
+        //{
+        //    //throw new System.NotImplementedException();
+        //}
+
+        //public void CompilationPipelineOnCompilationStarted(object compilationContext)
+        //{
+        //    //throw new System.NotImplementedException();
+        //}
+
+
     }
 }
