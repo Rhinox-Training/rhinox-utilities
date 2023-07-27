ee16597bcb31d79d68764978e2194a1799fc0d0f
Gaetan Deglorie <gaetan@rhinox.training>
2023-07-27 10:02:01 +0200

    [MOD] Icon editor in utilitiessettings

--- Assets/Utilities/Editor/Settings/EditorUtilitiesSettings.cs
+++ Assets/Utilities/Editor/Settings/EditorUtilitiesSettings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Rhinox.Lightspeed.Reflection;
 using Rhinox.Utilities.Attributes;
 using Sirenix.OdinInspector;
@@ -30,8 +31,78 @@ namespace Rhinox.Utilities.Editor
         
         [ListDrawerSettings, ShowIf(nameof(EnableCustomIconsInHierarchy))]
         public List<HierarchyIconSettingsEntry> Entries;
+        
+        [Button("Check for new icons")]
+        protected void UpdateIcons()
+        {
+            if (Entries == null)
+                Entries = new List<HierarchyIconSettingsEntry>();
+            
+            foreach (var kvp in CustomHierarchyDrawing.DefaultTypeIcons)
+            {
+                if (!kvp.Key.InheritsFrom(typeof(Component)) || kvp.Value == null)
+                    continue;
 
-        [Button("Reset Custom Icons")]
+                var defaultTypeEntry = Entries.FirstOrDefault(x => x.Type == kvp.Key);
+                if (defaultTypeEntry != null)
+                {
+                    if (defaultTypeEntry.Icon != kvp.Value)
+                        defaultTypeEntry.Icon = kvp.Value;
+                    else
+                        continue;
+                }
+                else
+                {
+                    var entry = new HierarchyIconSettingsEntry(kvp.Key, kvp.Value);
+                    Entries.Add(entry);
+                }
+            }
+
+            var types = TypeCache.GetTypesWithAttribute<CustomHierarchyIconAttribute>();
+            foreach (Type type in types)
+            {
+                if (!type.InheritsFrom(typeof(Component)))
+                    continue;
+                
+                var curTypeEntry = Entries.FirstOrDefault(x => x.Type == type);
+                if (curTypeEntry != null)
+                {
+                    var attr = type.GetCustomAttribute<CustomHierarchyIconAttribute>();
+                    var texture = attr.FindIcon();
+
+                    if (texture == null)
+                        continue;
+                    
+                    if (curTypeEntry.Icon != texture)
+                    {
+                        curTypeEntry.Icon = texture;
+                    }
+                    else
+                        continue;
+                }
+                else
+                {
+                    var attr = type.GetCustomAttribute<CustomHierarchyIconAttribute>();
+                    var texture = attr.FindIcon();
+                    if (texture == null)
+                        continue;
+                    var entry = new HierarchyIconSettingsEntry(type, texture);
+                    Entries.Add(entry);
+                }
+            }
+
+        }
+
+        [Button("Reset custom icons")]
+        protected void LoadCustomIconDefaultsButton()
+        {
+            if (EditorUtility.DisplayDialog("Confirmation", "Do you wish to reset the custom icon configuration?",
+                    "Confirm"))
+            {
+                LoadCustomIconDefaults();
+            }
+        }
+        
         protected void LoadCustomIconDefaults()
         {
             Entries = new List<HierarchyIconSettingsEntry>();
