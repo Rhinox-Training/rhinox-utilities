2695e1b25247988ff02370323b8d5ee28292f991
Gaetan Deglorie <gaetan@rhinox.training>
2023-12-01 14:11:02 +0100

    [ADD] Test class

---
+++ Assets/Tests/TestBetterEvents.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Rhinox.Utilities;
+using UnityEngine;
+
+public class TestBetterEvents : MonoBehaviour
+{
+    public BetterEvent Event;
+}
